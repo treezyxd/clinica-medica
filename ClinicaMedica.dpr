7a2d242d13ba213d23a3d89a11fce24f46de4ae8
treezy <thalesmaltaxd@gmail.com>
2023-08-20 12:26:29 -0300

    finalizado a primeira view

---
+++ ClinicaMedica.dpr
@@ -0,0 +1,14 @@
+program ClinicaMedica;
+
+uses
+  Vcl.Forms,
+  uPrincipal in 'src\views\uPrincipal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
